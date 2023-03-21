2f80ab5c5f27c359d16cc9e0598967c026008f05
AnnekkeH <112107957+AnnekkeH@users.noreply.github.com>
2023-03-21 20:56:56 +0000

    first attempt at user input

--- game.py
+++ game.py
@@ -1,66 +1,37 @@
-UserChoices = [{"U", "UP"}, {"R", "RIGHT"}, {"D", "DOWN"}, {"L", "LEFT"}, {"S", "SEARCH"}, {"M", "MISSION"}, {"U", "USE"}, {"I", "INVENTORY"}]
-UserInput = 0 # ALWAYS .UPPER UserInput
+UserChoices = ["U", "UP", "R", "RIGHT", "D", "DOWN", "L", "LEFT", "S", "SEARCH", "M", "MISSION", "U", "USE", "I", "INVENTORY"]
+UserInput = "0" # ALWAYS .UPPER UserInput
+Inventory = []
 
-def Intro(): # prints intro to game, add more description
+def intro(): # prints intro to game, add more description
 	print("""
-You awake from your sleeping pod and you are surrounded by bright, flashing lights. There is an alarm going off. You hear a robot-like voice speak to you.
+ You awake from your sleeping pod and you are surrounded by bright, flashing lights. There is an alarm going off. You hear a robot-like voice speak to you.
 
-“Wake up Mechanic! Something happened!"
-""")
-	input("Press Enter.\n")
+ “Wake up Mechanic! Something happened!"
+ """)
+	input("Press Enter.")
 	print("""
-"The Super Repair Laser broke in the Command Room! The parts are scattered across the space ship!"
-""")
-	input("Press Enter.\n")
+ "The Super Repair Laser broke in the Command Room! The parts are scattered across the space ship!"
+ """)
+	input("Press Enter.")
 
-def Mission(): # Prints mission from start of game
-	print("""
-The Super Repair Laser needs Gear, Spark Plug, Light Emitting Diode, and Duct Tape. They are located in the following areas:
-	Gear            >> Engine Room
-	Spark Plug      >> Barracks
-	LED             >> Break Room
-	Duct Tape       >> Ship Dock
+def mission(): # Prints mission from start of game
+	print("\mThe Super Repair Laser needs Gear, Spark Plug, Light Emitting Diode, and Duct Tape. They are located in the following areas:")
+	print("Gear            >> Engine Room")
+	print("Spark Plug      >> Barracks")
+	print("LED             >> Break Room")
+	print("Duct Tape       >> Ship Dock")
+	print("–type “Mission” or “M” at any time to bring this menu back–")
 
-–type “Mission” or “M” at any time to bring this menu back–""")
-
-def Help(): # prints Help Menu (commands)
-	print("""
-COMMANDS:\t\t\tAll can be shortened to First Letter
-	Up
-	Right
-	Down
-	Left
-	Search
-	Mission
-	Use
-	Inventory
-	""") # make hint function with room functions
-
-def Choice(): # what are you going to do mechanic
-	UserInput = input("What are you going to do Mechanic?\n")
-	UserInput = UserInput.upper
+def choice(UserInput): # checks if user choice is valid
+	while UserInput not in UserChoices:
+		print("Not Valid input")
+		UserInput = input("What are you going to do Mechanic?\n")
 	return UserInput
 
-Intro()
-Mission()
-Choice()
-
-# doesnt work for some reason, only to valid input
-if UserInput == UserChoices[0]:
-	print("up works")
-elif UserInput == UserChoices[1]:
-	print("right works")
-elif UserInput == UserChoices[2]:
-	print("down works")
-elif UserInput == UserChoices[3]:
-	print("left works")
-elif UserInput == UserChoices[4]:
-	print("search works")
-elif UserInput == UserChoices[5]:
-	print("mission works")
-elif UserInput == UserChoices[6]:
-	print("use works")
-elif UserInput == UserChoices[7]:
-	print("inventory works")
-else:
-	print("Not A Valid Input.")
+UserInput = input("What are you going to do Mechanic?\n")
+choice(UserInput)
+print(UserInput) # sees if its saving the input right
+# get user input
+# depending on user input, run room function
+# need to track location
+# room function depends on location
