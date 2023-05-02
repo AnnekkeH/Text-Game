115cfe70e113975b7ab7ba452dbf51e4e67ce80f
AnnekkeH <112107957+AnnekkeH@users.noreply.github.com>
2023-05-02 20:55:24 +0000

    May 2nd

--- game.py
+++ game.py
@@ -1,24 +1,81 @@
 import random
+import pickle
 
 UserChoices = ["U", "UP", "R", "RIGHT", "D", "DOWN", "L", "LEFT", "S", "SEARCH", "M", "MISSION", "U", "USE", "I", "INVENTORY", "Q", "QUIT"]
 UserInput = "test" # ALWAYS .UPPER UserInput
-Inventory = ["Gear", "Spark Plug"]
+Inventory = []
+info = Inventory
+BeenHereBefore = False
+
+def mainMenu(): # bad bad programming, bad logic here too, it's procrastination's child
+	global info
+	choice = input("""
+	The Game!
+----------------------------
+N - Start New Game
+L - Load Saved Game
+S - Save Current Game
+Q - Quit
+	\n""").upper()
+	while choice != 'Q':
+		if choice == 'N': # start new file
+			with open(f"saveFile.dat", "wb") as file: # write it
+				print("New File Created!\n") # add inventory and room status
+			intro()
+			break
+		elif choice == 'L': # load existing file
+			try:
+				with open(f"saveFile.dat", 'rb') as file:
+					inventory = pickle.load(file)
+					print(inventory) # checks if loading works
+					break
+			except FileNotFoundError as e:
+				print("Game file not found!")
+				print("Try saving first, or opening a new game.")
+				print(e)
+				choice = input("""
+	The Game!
+----------------------------
+N - Start New Game
+L - Load Saved Game
+S - Save Current Game
+Q - Quit
+	\n""").upper()
+		elif choice == 'S': # save current file, DONT FORGET TO SAVE BeenHereBefore
+			try:
+				with open('saveFile.dat', 'wb') as file:
+					pickle.dump(info, f) # DOES NOT WORK
+			except:
+				print("Current Game Cannot Save") # ENTERS HERE THEN LEAVES
+				choice = input("""
+	The Game!
+----------------------------
+N - Start New Game
+L - Load Saved Game
+S - Save Current Game
+Q - Quit
+	\n""").upper()
+		else:
+			print("Not Valid Input")
+			choice = input("""
+	The Game!
+----------------------------
+N - Start New Game
+L - Load Saved Game
+S - Save Current Game
+Q - Quit
+	\n""").upper()
 
 def intro(): # prints intro to game, add more description
 	print("You are a mechanic on one of the 'grandest' interstellar flights in history.\n")
-	print("You must replace a valve on an ice miner on asteriod AST-R10B\n")
-	input("Press Enter.")
+	print("You must replace a valve in an ice miner on asteriod AST-R10B\n")
+	input("Press Enter.\n")
 	# something to elaborate just a tiny amount, then be like "suddenly"
-	print("""
-You awake from your sleeping pod and you are surrounded by bright, flashing lights with an alarm going off. You hear a robot-like voice speak to you.
-
-“Wake up Mechanic! Something happened!"
- """)
-	input("Press Enter.")
-	print("""
-"The Super Repair Laser broke in the Command Room! The parts are scattered across the space ship!"
-""")
-	input("Press Enter.")
+	print("You awake from your sleeping pod and you are surrounded by bright, flashing lights with an alarm going off. You hear a robot-like voice speak to you.\n")
+	print("Wake up Mechanic! Something happened!\n")
+	input("Press Enter.\n")
+	print("The Super Repair Laser broke in the Command Room! The parts are scattered across the space ship!\n")
+	input("Press Enter.\n")
 	mission()
 
 def mission(): # Prints mission from start of game
@@ -29,29 +86,17 @@ def mission(): # Prints mission from start of game
 	print("Duct Tape       >> Ship Dock (Left)")
 	print("--type “Mission” or “M” at any time to bring this menu back--")
 	print("--type “Quit” or “Q” at at any time to save and quit--")
+	choice()
 
-def inventory(): # DOES NOT WORK, REFUSES TO ITERATE THROUGH MY JANK INVENTORY
+def inventory(): # HAVE INSTRUCTIONS TO OPEN INVENTORY WHEN FIRST FIND OBJECT
 	inventoryList = " "
 	print("You have the following in your inventory:")
-	for i in range(len(Inventory)): # list is not iritable for some reason
+	for i in range(len(Inventory)):
 		if i == "":
 			print("Nothing!!")
 			print("Run around to find the missing parts! Use H or Help if you are lost.")
 		print(Inventory[i])
 	return inventoryList
-
-def quit():
-	confirm = input("Are you sure you want to save and quit? (Yes/No)").upper() # confirm quit
-	if 'Y' in confirm:
-		name = input("What do you want to name your save file?") # get name of file
-		with open(f"{name}.txt", "w") as file: # write it
-		file.write(f"{inventory}\n") # add info (inventory, active room location, parts)
-	elif 'N' in confirm:
-		print("Okay! Have fun!")
-	else:
-		print("Not Valid Input")
-	# save game to file
-	# quit
 
 def choice(): # checks if user choice is valid
 	UserInput = input("What are you going to do Mechanic?\n").upper()
@@ -63,65 +108,69 @@ def choice(): # checks if user choice is valid
 		mission()
 	elif UserInput == UserChoices[14] or UserInput == UserChoices[15]: # if user input is inventory
 		inventory()
-	if UserInput == UserChoices[16] or UserInput == UserChoices[17]:
+	elif UserInput == UserChoices[16] or UserInput == UserChoices[17]:
 		quit()
+	# visually pleasing pause
+	if UserInput == 'U':
+		engine_room()
+	elif UserInput == 'R':
+		barracks()
+	elif UserInput == 'D':
+		break_room()
+	elif UserInput == 'L':
+		ship_dock()
 	return UserInput
 
-class Rooms(object):
-	Enter = False # one way flag for first entering room
-	Parts = [] # list of plot important parts
-	# need to make loot function that randomizes loot and adds it to parts
-	
-	def __init__(self, name, parts, loot):
-		Enter = True
-		self.RoomName = name
-		self.Parts = parts # not sure i need these, might make loot a function that adds to parts
+def quit():
+	confirm = input("Are you sure you want to save and quit? (Yes/No)\n").upper() # confirm quit
+	if 'Y' in confirm:
+		mainMenu()
+	elif 'N' in confirm:
+		print("Okay! Have fun!")
+	else:
+		print("Not Valid Input")
 
-		roomText = random.randrange(1, 4) # pick random text for first entering room
-		if roomText == 1:
-			print("room text 1")
-		elif roomText == 2:
-			print("room text 2")
-		elif roomText == 3:
-			print("room text 3")
-		else:
-			"You dun goofed"
+# -- make each room a function, only call function based on user input in choice() --
+def engine_room():
+	global BeenHereBefore # need to make each room its own flag, need to also save to file
+	if BeenHereBefore == False:
+		print("You have entered the Engine Room! Annekke forgot to add descriptive text!") # describe command room door add emphasis to searchable thing
+		BeenHereBefore = True
+	elif BeenHereBefore == True:
+		print("You've Been Here Before! Annekke forgot to add descriptive text!")
+	# search it
+	# add to inventory
+	# describe leaving
+	# leave, return inventory
+	choice()
 
-	def __str__(self):
-		pass
-		# print room text with like 2 variations
-	# room have text
-	# room have value for loot
-	# room have random for the random items
+def barracks():
+	print("You have entered the Barracks! Annekke forgot to add descriptive text!")
+	choice()
 
-class Hallways(Rooms):
-	pass
-	# use same Init code with different input text, have like 8 possible text descriptions
+def break_room():
+	print("You have entered the Break Room! Annekke forgot to add descriptive text!")
+	choice()
 
-# need to make save option
+def ship_dock():
+	print("You have entered the Ship Dock! Annekke forgot to add descriptive text!")
+	choice()
 
 def end_game():
-	pass
 	if Gear in inventory and SparkPlug in inventory and LED in inventory and DuctTape in inventory: # if plot important parts are in inventory
 		print("woo")
 		# describe command room door opening
 		# you see the broken repair laser
 		# you fix it
-		# dragon
-		# pre-scripted fight
 	else: # else
-		print("You are still missing required items! Keep exploring!") # tell player to collect items
+		print("You are still missing required items! Keep exploring!") # tell player to collect items, describe door not working properly
+
+# THIS IS THE CLASS LINE -----------------------------------------------------------
+
+class infoToSave(object):
+
+	def __init__(self):
+		self.inventory = inventory
 
 # RUN GAME ----------------------------------------------------------------------------------------------------------------------------------------
-# intro()
-UserInput = choice()
-# print(UserInput) # CHECKS USER INPUT
-
-# if user input == U:
-	# print engine room
-# elif user input == R:
-	# print barracks
-# elif user input == D:
-	# print Break room
-# elif user input = L:
-	# print Ship dock
+mainMenu()
